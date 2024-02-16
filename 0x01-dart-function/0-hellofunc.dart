62fea0dd84b093e7ce0ede7637bff0bd44157c9a
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-02-16 14:36:40 -0600

    fixing error

--- 0x01-dart-function/0-hellofunc.dart
+++ 0x01-dart-function/0-hellofunc.dart
@@ -1,7 +1,3 @@
-void helloFunction(String str) {
-  print('Hello $str from dart\n');
-}
-
-void main() {
-  helloFunction("Holberton");
+void helloFunction(String str){
+  print('Hello $str from dart');
 }
