e2b52fa037eb38d515d6bfa5b3a0a9763d5d64ac
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-02-24 14:51:26 -0600

    now i fixed the error

--- 0x01-dart-function/10-longest-palindrome.dart
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -1,7 +1,27 @@
-// Function to check if a string is a palindrome
+String longestPalindrome(String s) {
+  if (s.length < 3) {
+    return 'none';
+  }
+
+  String longest = '';
+
+  for (int i = 0; i < s.length; i++) {
+    for (int j = i + 2; j <= s.length; j++) {
+      String substring = s.substring(i, j);
+
+      if (isPalindrome(substring) && substring.length > longest.length) {
+        longest = substring;
+      }
+    }
+  }
+
+  return longest.isNotEmpty ? longest : 'none';
+}
+
 bool isPalindrome(String s) {
   int left = 0;
   int right = s.length - 1;
+
   while (left < right) {
     if (s[left] != s[right]) {
       return false;
@@ -9,32 +29,8 @@ bool isPalindrome(String s) {
     left++;
     right--;
   }
+
   return true;
-}
-
-// Function to find the longest palindrome substring
-String longestPalindrome(String s) {
-  if (s.length < 3) {
-    return ""; // If the length of the string is less than 3, return an empty string
-  }
-
-  String longest = ""; // Initialize the longest palindrome substring found so far to an empty string
-
-  // Iterate through all possible substrings of the input string
-  for (int i = 0; i < s.length; i++) {
-    for (int j = i + 1; j <= s.length; j++) {
-      String substring = s.substring(i, j); // Extract the current substring
-      if (isPalindrome(substring)) {
-        // If the substring is a palindrome
-        if (substring.length > longest.length) {
-          // If the length of the current substring is greater than the length of the longest palindrome substring found so far
-          longest = substring; // Update the longest palindrome substring
-        }
-      }
-    }
-  }
-
-  return longest; // Return the longest palindrome substring found (could be an empty string if none is found)
 }
 
 void main() {
