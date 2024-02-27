94429f4c9f12f6b193800e26af584b87f63137d8
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-02-26 19:58:26 -0600

    adding task 6

---
+++ 0x02-dart-classes/6-inheritance.dart
@@ -0,0 +1,44 @@
+// File: 6-inheritance
+
+import '6-password.dart';
+
+class User extends Password {
+  int id;
+  String name;
+  int age;
+  double height;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.age,
+    required this.height,
+    String user_password = '',
+  }) : super(password: user_password);
+
+  String get user_password => this.password;
+  set user_password(String newPassword) => this.password = newPassword;
+
+  // Override toJson to include user_password
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+      'height': height,
+    };
+  }
+
+  // fromJson method to create a User instance from a map.
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+        id: userJson['id'],
+        name: userJson['name'],
+        age: userJson['age'],
+        height: userJson['height'],
+        user_password: userJson['user_password']);}
+
+  // Override toString to include validation status of the password
+  @override
+  String toString() => 'User(id : $id ,name: $name, age: $age, height: $height, Password: ${isValid()})';
+}
---
+++ 0x02-dart-classes/6-password.dart
@@ -0,0 +1,26 @@
+// File: 6-password.dart
+
+class Password {
+  Password({required String password}) {
+    this._password = password;
+  }
+
+  /// a "private" password
+  late String _password;
+
+  String get password => _password;
+  set password(String pass) => _password = pass;
+
+  bool isValid() {
+    if (_password.length >= 8 && _password.length <= 16) {
+      RegExp exp = RegExp(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$");
+      return exp.hasMatch(_password);
+    }
+    return false;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: $_password';
+  }
+}
